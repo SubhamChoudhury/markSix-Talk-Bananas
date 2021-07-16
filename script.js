ce5e6bce009d4a9caba186aab4d28b7797e9776c
SubhamChoudhury <subhamchoudhury2408@gmail.com>
2021-07-16 19:05:12 +0530

    code: API added :+1:

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
+alert("You can only use the app for 5 times in an hour, so use is wisely.");
 var translateBtn = document.querySelector("#translate-btn");
 var textInput = document.querySelector("#text-input");
 var outputDiv = document.querySelector("#output");
+var serverURL = "https://api.funtranslations.com/translate/minion.json" //API
 
-translateBtn.addEventListener("click", function clickEventHandler(){
-    outputDiv.innerHTML = "translate" + textInput.value;
+function getTranslationURL(text) {
+    return serverURL + "?" + "text=" + text;
+}
+function errorHandler(error) {
+    console.log("Error encountered", error);
+    alert("Something went wrong with the server! Please try again later");
+}
+
+
+translateBtn.addEventListener("click", function clickEventHandler() {
+    var inputText = textInput.value;    //taking input
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText;  //receiving output
+        })
+        .catch(errorHandler)
 })
